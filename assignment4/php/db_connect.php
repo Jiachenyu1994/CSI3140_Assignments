6d9d9dcd52182031a5de63dba1dcb0738bedbef8
Jiachen Yu <stephenyu1994@gmail.com>
2024-07-17 19:41:04 -0400

    test

---
+++ assignment4/php/db_connect.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$db = 'emergency_waitlist';
+$user = 'postgres';  // or your newly created user
+$password = 'admin';  // ensure this is the correct password
+
+try {
+    $dsn = "pgsql:host=$host;port=5432;dbname=$db;";
+    $pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+    exit;  // Ensure the script exits if the connection fails
+}
+?>
